f31cd689baa9143a3460998eac2cccb5143528f4
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2024-10-13 01:37:12 +0000

    Adicionada rota de Get e Controller para recurso Article

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+  get '/articles', to: 'articles#index'
+
+
+
   # root "posts#index"
 end
